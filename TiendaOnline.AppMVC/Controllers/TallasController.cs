06e9fa7ace4e079ba7f01bd56ec5165338369eab
Gabriel <gabrielobandogarcia100@gmail.com>
2026-03-18 01:26:45 -0600

    TIEN-46 featGestión de Inventario

---
+++ TiendaOnline.AppMVC/Controllers/TallasController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TiendaOnline.AppMVC.Models;
+
+namespace TiendaOnline.AppMVC.Controllers
+{
+    public class TallasController : Controller
+    {
+        private readonly TiendaOnlineZapContext _context;
+
+        public TallasController(TiendaOnlineZapContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tallas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tallas.ToListAsync());
+        }
+
+        // GET: Tallas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var talla = await _context.Tallas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (talla == null)
+            {
+                return NotFound();
+            }
+
+            return View(talla);
+        }
+
+        // GET: Tallas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tallas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Numero,Estatus,FechaCreacion")] Talla talla)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(talla);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(talla);
+        }
+
+        // GET: Tallas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var talla = await _context.Tallas.FindAsync(id);
+            if (talla == null)
+            {
+                return NotFound();
+            }
+            return View(talla);
+        }
+
+        // POST: Tallas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Numero,Estatus,FechaCreacion")] Talla talla)
+        {
+            if (id != talla.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(talla);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TallaExists(talla.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(talla);
+        }
+
+        // GET: Tallas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var talla = await _context.Tallas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (talla == null)
+            {
+                return NotFound();
+            }
+
+            return View(talla);
+        }
+
+        // POST: Tallas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var talla = await _context.Tallas.FindAsync(id);
+            if (talla != null)
+            {
+                _context.Tallas.Remove(talla);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TallaExists(int id)
+        {
+            return _context.Tallas.Any(e => e.Id == id);
+        }
+    }
+}
--- TiendaOnline.AppMVC/Models/Talla.cs
+++ TiendaOnline.AppMVC/Models/Talla.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TiendaOnline.AppMVC.Models;
 
@@ -7,6 +8,7 @@ public partial class Talla
 {
     public int Id { get; set; }
 
+    [Display(Name = "Talla")]
     public string Numero { get; set; } = null!;
 
     public byte Estatus { get; set; }
