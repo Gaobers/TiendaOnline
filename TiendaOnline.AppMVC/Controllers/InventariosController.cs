5ea391e633f21aac83b6bc39b4979ebff2c51e94
Gabriel <gabrielobandogarcia100@gmail.com>
2026-03-18 01:26:45 -0600

    TIEN-46 featGestión de Inventario

---
+++ TiendaOnline.AppMVC/Controllers/InventariosController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TiendaOnline.AppMVC.Models;
+
+namespace TiendaOnline.AppMVC.Controllers
+{
+    public class InventariosController : Controller
+    {
+        private readonly TiendaOnlineZapContext _context;
+
+        public InventariosController(TiendaOnlineZapContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Inventarios
+        public async Task<IActionResult> Index()
+        {
+            var tiendaOnlineZapContext = _context.Inventarios.Include(i => i.Producto).Include(i => i.Talla);
+            return View(await tiendaOnlineZapContext.ToListAsync());
+        }
+
+        // GET: Inventarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventario = await _context.Inventarios
+                .Include(i => i.Producto)
+                .Include(i => i.Talla)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (inventario == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventario);
+        }
+
+        // GET: Inventarios/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductoId"] = new SelectList(_context.Productos, "Id", "Nombre");
+            ViewData["TallaId"] = new SelectList(_context.Tallas, "Id", "Numero");
+            return View();
+        }
+
+        // POST: Inventarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProductoId,TallaId,Stock,StockMinimo")] Inventario inventario)
+        {
+            ModelState.Remove("Producto");
+            ModelState.Remove("Talla");
+
+            inventario.FechaActualizacion = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(inventario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductoId"] = new SelectList(_context.Productos, "Id", "Nombre", inventario.ProductoId);
+            ViewData["TallaId"] = new SelectList(_context.Tallas, "Id", "Numero", inventario.TallaId);
+            return View(inventario);
+        }
+
+        // GET: Inventarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventario = await _context.Inventarios.FindAsync(id);
+            if (inventario == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductoId"] = new SelectList(_context.Productos, "Id", "Nombre", inventario.ProductoId);
+            ViewData["TallaId"] = new SelectList(_context.Tallas, "Id", "Numero", inventario.TallaId);
+            return View(inventario);
+        }
+
+        // POST: Inventarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductoId,TallaId,Stock,StockMinimo,FechaActualizacion")] Inventario inventario)
+        {
+            if (id != inventario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(inventario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InventarioExists(inventario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductoId"] = new SelectList(_context.Productos, "Id", "Nombre", inventario.ProductoId);
+            ViewData["TallaId"] = new SelectList(_context.Tallas, "Id", "Numero", inventario.TallaId);
+            return View(inventario);
+        }
+
+        // GET: Inventarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var inventario = await _context.Inventarios
+                .Include(i => i.Producto)
+                .Include(i => i.Talla)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (inventario == null)
+            {
+                return NotFound();
+            }
+
+            return View(inventario);
+        }
+
+        // POST: Inventarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var inventario = await _context.Inventarios.FindAsync(id);
+            if (inventario != null)
+            {
+                _context.Inventarios.Remove(inventario);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InventarioExists(int id)
+        {
+            return _context.Inventarios.Any(e => e.Id == id);
+        }
+    }
+}
