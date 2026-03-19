278a1b827cf834b5d86654a30fd9eb0d8e1056d2
Rosaura <Karlarosauras@gmail.com>
2026-03-18 21:41:18 -0600

    Rosaura Creo una Validación

--- TiendaOnline.AppMVC/Models/Usuario.cs
+++ TiendaOnline.AppMVC/Models/Usuario.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace TiendaOnline.AppMVC.Models;
 
 public partial class Usuario
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage ="El nombre es obligatorio")]
     public string Nombre { get; set; } = null!;
 
     public string Correo { get; set; } = null!;
