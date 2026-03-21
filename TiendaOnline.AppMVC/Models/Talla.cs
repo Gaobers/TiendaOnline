f015d27bed6b030ebeca6476fd7a217a4169c603
Gabriel <gabrielobandogarcia100@gmail.com>
2026-03-20 23:07:56 -0600

    Validación de los campos de Tallas

--- TiendaOnline.AppMVC/Models/Talla.cs
+++ TiendaOnline.AppMVC/Models/Talla.cs
@@ -8,11 +8,17 @@ public partial class Talla
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "La talla es obligatoria.")]
+    [StringLength(10, MinimumLength = 1, ErrorMessage = "La talla debe tener entre 1 y 10 caracteres.")]
     [Display(Name = "Talla")]
     public string Numero { get; set; } = null!;
 
+
+    [Display(Name = "Estado")]
     public byte Estatus { get; set; }
 
+    [DataType(DataType.DateTime)]
+    [Display(Name = "Fecha de Creación")]
     public DateTime FechaCreacion { get; set; }
 
     public virtual ICollection<Inventario> Inventarios { get; set; } = new List<Inventario>();
