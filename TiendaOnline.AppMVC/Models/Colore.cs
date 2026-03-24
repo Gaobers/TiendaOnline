b736c5608a46b09985d436741d53bd1aa32480bd
Rosaura <Karlarosauras@gmail.com>
2026-03-24 08:52:57 -0600

    Nuevo cambio

--- TiendaOnline.AppMVC/Models/Colore.cs
+++ TiendaOnline.AppMVC/Models/Colore.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TiendaOnline.AppMVC.Models;
 
 public partial class Colore
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre del color es obligatorio.")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe tener entre 3 y 50 caracteres.")]
+    [RegularExpression(@"^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]+$", ErrorMessage = "El nombre solo puede contener letras.")]
     public string Nombre { get; set; } = null!;
 
+    [Display(Name = "Código Hexadecimal")]
     public string? CodigoHex { get; set; }
 
     public byte Estatus { get; set; }
