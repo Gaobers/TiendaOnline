12972b4378f129683ff1c6c97ef5b05fdb40c6b5
Gabriel <gabrielobandogarcia100@gmail.com>
2026-04-10 23:07:55 -0600

    Se crearon viewmodel para la parte publica del sistema

---
+++ TiendaOnline.AppMVC/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using TiendaOnline.AppMVC.Models;
+
+namespace TiendaOnline.AppMVC.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalProductos { get; set; }
+        public int TotalImagenes { get; set; }
+        public int TotalPedidos { get; set; }
+        public int TotalUsuarios { get; set; }
+
+        public List<Producto> ProductosRecientes { get; set; } = new();
+    }
+}
